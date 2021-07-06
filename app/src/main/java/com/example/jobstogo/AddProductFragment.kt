378c8ef55be7464de5812279686dc975d03824e1
maxstoney4 <maxstoney4@gmail.com>
2021-07-06 16:25:14 +0200

    addProduct fragment added/version 2

---
+++ app/src/main/java/com/example/jobstogo/AddProductFragment.kt
@@ -0,0 +1,55 @@
+package com.example.jobstogo
+
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.jobstogo.databinding.FragmentAddProductBinding
+import com.example.jobstogo.databinding.FragmentJobBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ShopDetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AddProductFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val binding = DataBindingUtil.inflate<FragmentAddProductBinding>(inflater,
+            R.layout.fragment_add_product, container, false)
+        val db = Firebase.firestore
+
+        // Create a new user with a first, middle, and last name
+        val user = hashMapOf(
+            "first" to "Alan",
+            "middle" to "Mathison",
+            "last" to "Turing",
+            "born" to 1912
+        )
+
+// Add a new document with a generated ID
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+
+
+        return binding.root
+    }
+}
